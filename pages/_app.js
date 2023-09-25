d1ffd17f149537874aa1dbca602331cdb7b7eedf
chimpdev <skyhancloud@gmail.com>
2023-09-25 08:01:34 +0300

    🎉 → Adding tooltip for theme changer

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 import '@/styles/globals.css';
 import NextNProgress from 'nextjs-progressbar';
 import 'tippy.js/dist/tippy.css';
-import Head from 'next/head';
+import Tippy from '@tippyjs/react';
 import { useThemeStore } from '@/stores';
 import { useEffect } from 'react';
 import { useCookie } from 'react-use';
 import { MdDarkMode } from 'react-icons/md';
 import { BiSolidSun } from 'react-icons/bi';
+import { CgDarkMode } from 'react-icons/cg';
 import '@/styles/prism.css';
 import '@/styles/prism-vsc-dark-plus.css';
+import { twMerge } from 'tailwind-merge';
 
 export default function App({ Component, pageProps }) {
 
@@ -40,13 +42,25 @@ export default function App({ Component, pageProps }) {
       </div>
 
       <div className='z-10 absolute top-0 left-0 w-full h-full flex justify-end items-end p-8 pointer-events-none'>
-        <div className='bg-light-tertiary dark:bg-dark-tertiary w-8 h-8 rounded-full hover:opacity-60 pointer-events-auto cursor-pointer fixed text-light-primaryText dark:text-dark-primaryText flex items-center justify-center' onClick={() => toggleTheme()}>
-          {theme === 'dark' ? (
-            <BiSolidSun />
-          ) : (
-            <MdDarkMode />
-          )}
-        </div>
+        <Tippy inertia={true} interactive={true} content={<div className='-mx-2 -my-0.5 flex flex-row gap-y-1 items-center justify-center pointer-events-auto text-light-primaryText dark:text-dark-primaryText'>
+          <div className={twMerge(
+            'w-max px-1.5 py-1 bg-[#e7e7e7] hover:bg-[#e2e2e2] dark:bg-[#181818] hover:dark:bg-[#202020] rounded-l cursor-pointer',
+            theme === 'dark' && 'pointer-evets-none opacity-60 cursor-default hover:bg-[#e7e7e7] dark:hover:bg-[#181818]'
+          )} onClick={() => setTheme('dark')}>
+            <MdDarkMode size={18} />
+          </div>
+
+          <div className={twMerge(
+            'w-max px-1.5 py-1 bg-[#e7e7e7] hover:bg-[#e2e2e2] dark:bg-[#181818] hover:dark:bg-[#202020] rounded-r cursor-pointer',
+            theme === 'light' && 'pointer-evets-none opacity-60 cursor-default hover:bg-[#e7e7e7] dark:hover:bg-[#181818]'
+          )} onClick={() => setTheme('light')}>
+            <BiSolidSun size={18} />
+          </div>
+        </div>}>
+          <div className='bg-light-tertiary dark:bg-dark-tertiary w-8 h-8 rounded-full hover:opacity-60 pointer-events-auto cursor-pointer fixed text-light-primaryText dark:text-dark-primaryText flex items-center justify-center'>
+            <CgDarkMode />
+          </div>
+        </Tippy>
       </div>
     </>
   );
