ad7a087950e4228faaff512b3ab79f3da4b553e3
chimpdev <skyhancloud@gmail.com>
2023-10-23 18:24:38 +0300

    style: Rearrange class names and add gap between flex items in Footer component

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -8,17 +8,17 @@ export default function Footer() {
   return (
     <>
       <Seperator />
-      <div className="mx-6 flex flex-col">
+      <div className="flex flex-col mx-6">
         <h1 className='text-lg'>
           Bana Ulaş
         </h1>
 
-        <div className='flex flex-row text-sm text-light-tertiaryText dark:text-dark-tertiaryText mt-2'>
+        <div className='flex flex-col gap-2 mt-2 text-sm sm:flex-row text-light-tertiaryText dark:text-dark-tertiaryText sm:gap-0'>
           {social.map((item, index) => (
             <Link className={twMerge(
               'w-full py-2 text-center hover:opacity-80 cursor-pointer font-semibold social-button',
-              index === 0 ? 'rounded-lg rounded-r-none' : '',
-              index === social.length - 1 ? 'rounded-xl rounded-l-none' : '',
+              index === 0 ? 'rounded-xl sm:rounded-r-none' : '',
+              index === social.length - 1 ? 'rounded-xl sm:rounded-l-none' : '',
             )} style={{ backgroundColor: item.color + '20', color: item.color, '--color': item.color + '50' }} key={index} href={item.url} target='_blank'>
               {item.name}
               <LuArrowUpRight className='inline-block ml-1' />
@@ -26,7 +26,7 @@ export default function Footer() {
           ))}
         </div>
 
-        <Link href='https://github.com/chimpdev/bencan.net' target='_blank' className='w-full py-2 text-center hover:opacity-80 cursor-pointer font-medium rounded mt-2 hover:bg-light-tertiary hover:dark:bg-dark-tertiary text-light-primaryText dark:text-dark-primaryText text-sm'>
+        <Link href='https://github.com/chimpdev/bencan.net' target='_blank' className='w-full py-2 mt-2 text-sm font-medium text-center rounded cursor-pointer hover:opacity-80 hover:bg-light-tertiary hover:dark:bg-dark-tertiary text-light-primaryText dark:text-dark-primaryText'>
           Bu website <LuGithub className='inline-block ml-1' /> GitHub'da açık kaynak kodludur.
         </Link>
       </div>
