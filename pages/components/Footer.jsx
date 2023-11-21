3cee53b4ca61882ce106ba500368b76c23d99ca5
chimpdev <skyhancloud@gmail.com>
2023-11-21 21:58:23 +0300

    Refactor social links in Footer component

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -1,7 +1,6 @@
 import Seperator from './Seperator';
 import social from '@/data/social.json';
 import { LuArrowUpRight, LuGithub } from 'react-icons/lu';
-import { twMerge } from 'tailwind-merge';
 import Link from 'next/link';
 
 export default function Footer() {
@@ -13,23 +12,19 @@ export default function Footer() {
           Bana Ulaş
         </h1>
 
-        <div className='flex flex-col gap-2 mt-2 text-sm sm:flex-row text-light-tertiaryText dark:text-dark-tertiaryText sm:gap-0'>
-          {social.map((item, index) => (
-            <Link className={twMerge(
-              'w-full py-2 text-center hover:opacity-80 cursor-pointer font-semibold social-button',
-              index === 0 && 'rounded-xl sm:rounded-r-none',
-              index === social.length - 1 && 'rounded-xl sm:rounded-l-none',
-              index !== 0 && index !== social.length - 1 && 'rounded-xl sm:rounded-none',
-            )} style={{ backgroundColor: item.color + '20', color: item.color, '--color': item.color + '50' }} key={index} href={item.url} target='_blank'>
+        <div className='flex flex-wrap w-full gap-2 mt-2 text-sm text-light-tertiaryText dark:text-dark-tertiaryText'>
+          {social.map(item => (
+            <Link className='flex items-center justify-center flex-1 w-full px-4 py-2 border-2 cursor-pointer gap-x-1 text-light-primaryText dark:text-dark-primaryText rounded-xl gap-y-2 bg-light-secondary dark:bg-dark-secondary border-light-tertiary dark:border-dark-tertiary hover:dark:bg-dark-tertiary hover:bg-light-tertiary' href={item.url} target='_blank'>
               {item.name}
-              <LuArrowUpRight className='inline-block ml-1' />
+              <LuArrowUpRight />
             </Link>
           ))}
-        </div>
 
-        <Link href='https://github.com/chimpdev/bencan.net' target='_blank' className='w-full py-2 mt-2 text-sm font-medium text-center rounded cursor-pointer hover:opacity-80 hover:bg-light-tertiary hover:dark:bg-dark-tertiary text-light-primaryText dark:text-dark-primaryText'>
-          Bu website <LuGithub className='inline-block ml-1' /> GitHub'da açık kaynak kodludur.
-        </Link>
+          <Link className='flex items-center justify-center flex-1 w-full px-4 py-2 border-2 cursor-pointer gap-x-1 text-light-primaryText dark:text-dark-primaryText rounded-xl gap-y-2 bg-light-secondary dark:bg-dark-secondary border-light-tertiary dark:border-dark-tertiary hover:dark:bg-dark-tertiary hover:bg-light-tertiary' href='https://github.com/chimpdev/bencan.net'>
+            Source
+            <LuGithub />
+          </Link>
+        </div>
       </div>
     </>
   );
