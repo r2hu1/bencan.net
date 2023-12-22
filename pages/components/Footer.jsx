8cc6b121649783b26f6409d5fefa32cbba1d761a
Gökhan Can Bulut <55600314+chimpdev@users.noreply.github.com>
2023-12-23 02:24:02 +0300

    Update Footer.jsx

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
           Contact
         </h1>
 
-        <div className='flex flex-wrap w-full gap-2 mt-2 text-sm text-light-tertiaryText dark:text-dark-tertiaryText'>
+        <div className='flex flex-col flex-wrap w-full gap-2 mt-2 text-sm sm:flex-row text-light-tertiaryText dark:text-dark-tertiaryText'>
           {social.map(item => (
             <Link className='flex min-w-[200px] items-center justify-center flex-1 w-full px-4 py-2 border-2 cursor-pointer gap-x-1 text-light-primaryText dark:text-dark-primaryText rounded-xl gap-y-2 bg-light-secondary dark:bg-dark-secondary border-light-tertiary dark:border-dark-tertiary hover:dark:bg-dark-tertiary hover:bg-light-tertiary' href={item.url} target='_blank'>
               {item.name}
