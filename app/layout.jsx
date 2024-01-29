5ec5bb102cad8a25fbe6dcfc66644c385673a4b7
chimpdev <skyhancloud@gmail.com>
2024-01-29 12:09:39 +0300

    chore: Remove react lenis

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,6 @@ import { Inter } from 'next/font/google';
 import { Toaster } from '@/components/ui/sonner';
 import '@/styles/tailwind.css';
 import '@/styles/main.css';
-import { ReactLenis } from '@studio-freight/react-lenis';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,16 +24,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <ReactLenis root>
-        <body className={inter.className}>
-          <div className='flex justify-center w-full h-full relative z-[1]'>
-            <main className='max-w-[1200px] w-full h-[100dvh] px-16 sm:px-20 xl:px-0'>
-              {children}
-            </main>
-            <Toaster />
-          </div>
-        </body>
-      </ReactLenis>
+      <body className={inter.className}>
+        <div className='flex justify-center w-full h-full relative z-[1]'>
+          <main className='max-w-[1200px] w-full h-[100dvh] px-16 sm:px-20 xl:px-0'>
+            {children}
+          </main>
+          <Toaster />
+        </div>
+      </body>
     </html>
   );
 }
