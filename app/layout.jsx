0738b224ad5361b1e9e1b84cf6a0def607e5592c
chimpdev <skyhancloud@gmail.com>
2024-01-29 12:00:19 +0300

    feat(layout): add ReactLenis wrapper and import ReactLenis library

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import { Toaster } from '@/components/ui/sonner';
 import '@/styles/tailwind.css';
 import '@/styles/main.css';
+import { ReactLenis } from '@studio-freight/react-lenis';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,14 +25,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <div className='flex justify-center w-full h-full relative z-[1]'>
-          <main className='max-w-[1200px] w-full h-[100dvh] px-16 sm:px-20 xl:px-0'>
-            {children}
-          </main>
-          <Toaster />
-        </div>
-      </body>
+      <ReactLenis root>
+        <body className={inter.className}>
+          <div className='flex justify-center w-full h-full relative z-[1]'>
+            <main className='max-w-[1200px] w-full h-[100dvh] px-16 sm:px-20 xl:px-0'>
+              {children}
+            </main>
+            <Toaster />
+          </div>
+        </body>
+      </ReactLenis>
     </html>
   );
 }
