a88e8e571e0046551554e14d870f3d419f36da2b
chimpdev <skyhancloud@gmail.com>
2024-01-30 13:50:59 +0300

    feat(layout): Add ReactLenis wrapper to improve scrolling experience and touch synchronization

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Inter } from 'next/font/google';
 import { Toaster } from '@/components/ui/sonner';
+import { ReactLenis } from '@studio-freight/react-lenis';
 import '@/styles/tailwind.css';
 import '@/styles/main.css';
 
@@ -20,18 +23,22 @@ export const metadata = {
     icon: '/favicon.ico'
   }
 };
-
+  
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <div className='flex justify-center w-full h-full relative z-[1]'>
-          <main className='max-w-[1200px] w-full h-[100dvh] px-16 sm:px-20 xl:px-0'>
-            {children}
-          </main>
-          <Toaster />
-        </div>
-      </body>
+      <ReactLenis root options={{
+        syncTouch: true
+      }}>
+        <body className={inter.className}>
+          <div className='flex justify-center w-full h-full relative z-[1]'>
+            <main className='max-w-[1200px] w-full h-[100dvh] px-16 sm:px-20 xl:px-0'>
+              {children}
+            </main>
+            <Toaster />
+          </div>
+        </body>
+      </ReactLenis>
     </html>
   );
 }
